5f025ba0be943d857ea56e2fb266a8f2484dfd1b
DIMA <dmitriychebotarev78@mail.ru>
2024-06-16 21:49:46 +0300

    revers number

--- rever_number/main.cpp
+++ rever_number/main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	cout<<"\thello revers numbers\n";
-	
+  int n=0, reversedNumber =0, remainder=0;
+  cout<<"enter intenger number\n";
+  cin >>n;
+  
+  while (n !=0)	{
+  	remainder =n%10;
+  	reversedNumber = reversedNumber*10 + remainder;
+  	n/=10;
+  }
+  cout<<"Revers number = "<<reversedNumber<<endl;
 	return 0;
 }
