4e275896cca3c8fa4a4827f245639a0811a6ae0a
DIMA <dmitriychebotarev78@mail.ru>
2024-06-16 21:56:45 +0300

    addad main.cpp

--- rever_number/main.cpp
+++ rever_number/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
-using namespace std;
+#include <limits> // Для std::numeric_limits
 
 int main(int argc, char** argv) {
-	cout<<"\thello revers numbers\n";
-  int n=0, reversedNumber =0, remainder=0;
-  cout<<"enter intenger number\n";
-  cin >>n;
-  
-  while (n !=0)	{
-  	remainder =n%10;
-  	reversedNumber = reversedNumber*10 + remainder;
-  	n/=10;
-  }
-  cout<<"Revers number = "<<reversedNumber<<endl;
-	return 0;
+    std::cout << "\tthe hello revers numbers\n";
+    int n = 0, reversedNumber = 0, remainder = 0;
+    std::cout << "\tEnter an integer number: ";
+    
+    // Проверка ввода
+    while (!(std::cin >> n)) {
+        std::cin.clear(); // Очистка флага ошибки
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), 'n'); // Пропуск неверного ввода
+        std::cout << "\tInvalid input. Please enter an integer number: ";
+    }
+
+    // Переворачивание числа
+    while (n != 0) {
+        remainder = n % 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+        n /= 10;
+    }
+
+    std::cout << "\tReversed number = " << reversedNumber << std::endl;
+    return 0;
 }
